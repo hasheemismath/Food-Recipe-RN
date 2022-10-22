6749cdfa94095ca796eeed61cff553ed0bf9ae35
hasheemismath <hasheemhush@gmail.com>
2022-10-22 15:19:17 +0530

    pushing to repo

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import RecipeDetailScreen from "./Screens/Restaurant/RecipeDetailScreen";
+import DATA from "./config/Restaurant/DATA";
+import WelcomeScreen from "./Screens/Restaurant/WelcomeScreen";
+import HomeScreen from "./Screens/Restaurant/HomeScreen";
+import {View} from "react-native";
+const App = () => {
+  return <WelcomeScreen/>
+
+
+
+
+};
+
+
+export default App;
